170da29fe7d67aaba24e744e169da3486a4acff0
dobry <dobryjartem738@gmail.com>
2024-12-11 20:15:10 +0300

    commit with tests

--- app/src/androidTest/java/com/example/myapplication1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication1/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication1
 
+import android.view.View
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,48 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myapplication1", appContext.packageName)
+    fun testCurrencyConversion() {
+
+        Espresso.onView(withId(R.id.amountEditText)).perform(ViewActions.typeText("100"))
+
+
+        Espresso.onView(withId(R.id.fromCurrencySpinner)).perform(ViewActions.click())
+        Espresso.onView(withText("USD")).perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.toCurrencySpinner)).perform(ViewActions.click())
+        Espresso.onView(withText("EUR")).perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.convertButton)).perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.resultTextView))
+            .check(ViewAssertions.matches(withText("Ответ: 92.00 EUR")))
+    }
+
+    @Test
+    fun testInvalidCurrencyConversion() {
+
+        Espresso.onView(withId(R.id.amountEditText)).perform(ViewActions.typeText("100"))
+
+
+        Espresso.onView(withId(R.id.fromCurrencySpinner)).perform(ViewActions.click())
+        Espresso.onView(withText("USD")).perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.toCurrencySpinner)).perform(ViewActions.click())
+        Espresso.onView(withText("RUB")).perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.convertButton)).perform(ViewActions.click())
+
+
+        Espresso.onView(withId(R.id.resultTextView))
+            .check(ViewAssertions.matches(withText("Ответ: 8500.00 RUB")))
     }
 }
--- app/src/test/java/com/example/myapplication1/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication1/ExampleUnitTest.kt
@@ -1,17 +1,41 @@
 package com.example.myapplication1
 
 import org.junit.Test
-
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+    private val exchangeRates = mapOf(
+        "USD" to 1.0,
+        "EUR" to 0.92,
+        "CHF" to 0.91,
+        "RUB" to 85.0
+    )
+
+    private fun convertCurrency(amount: Double, fromCurrency: String, toCurrency: String): Double {
+        val fromRate = exchangeRates[fromCurrency]
+        val toRate = exchangeRates[toCurrency]
+
+        if (fromRate == null || toRate == null) {
+            return amount
+        }
+
+        return (amount / fromRate) * toRate
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testCurrencyConversion() {
+        val amount = 100.0
+
+
+        val result = convertCurrency(amount, "USD", "EUR")
+        assertEquals(92.0, result, 0.01)
+
+
+        val result2 = convertCurrency(amount, "EUR", "RUB")
+
+
+        val expectedResult = (amount / 0.92) * 85.0
+        assertEquals(expectedResult, result2, 0.01)
     }
 }
+
